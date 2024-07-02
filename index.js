fec423d02f0da130b1b872b3361cd1db1ddb5e4e
Nwosu Chidinma <chidinmaifynwosu@gmail.com>
2024-07-02 13:33:17 +0100

    It is a single page application, so i had to delete the nav bar

---
+++ index.js
@@ -0,0 +1,33 @@
+// const slackDisplayName = "Chidinma Nwosu";  
+// const slackEmail = "chidinmaifynwo@gmail.com";  
+// const slackProfilePicture = "./assets/images/profile-pic2.png";  
+
+  function updateTimeAndDay(){
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    const dayNames= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const day = dayNames[now.getDay()];
+
+    //format minutes to always be two digits
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+
+    //format hours to be either AM or PM
+    const amPmSuffix = hours >=12 ? 'pm' : 'am';
+    hours= hours % 12;
+    hours = hours ? hours : 12;
+    const timestr = hours + ':' + minutes + amPmSuffix;
+
+    //update the HTML elements
+    const timeElement= document.getElementById('currentTimeUTC');
+    const dayElement= document.getElementById('currentDay');
+    if(timeElement)timeElement.textContent = timestr;
+    if(dayElement)dayElement.textContent = day;
+  }
+
+  //update the time and day every second
+  setInterval(updateTimeAndDay, 1000);
+
+
+
+    
